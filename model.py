b38d0c14d0038ca0ff2fa8bd6aff5eb4d2409104
Vitchyr Pong <vitchyr@gmail.com>
2020-08-21 22:21:14 -0700

    added pnp experiments

--- model.py
+++ model.py
@@ -21,21 +21,21 @@ class UnFlatten(nn.Module):
     def __init__(self, n_channels):
         super(UnFlatten, self).__init__()
         self.n_channels = n_channels
-    
+
     def forward(self, input):
         size = int((input.size(1) // self.n_channels) ** 0.5)
         return input.view(input.size(0), self.n_channels, size, size)
 
 
 class ConvVAE(nn.Module):
-    def __init__(self, device='cuda', img_channels=3, args=None):
+    def __init__(self, device='cuda', img_channels=3, args=None, img_size=28):
         super().__init__()
         self.batch_size = args.batch_size
         self.device = device
         self.z_dim = 20
         self.img_channels = img_channels
         self.model = args.model
-        img_size = 28
+        img_size = img_size
         filters_m = 32
 
         ## Build network
@@ -45,7 +45,7 @@ class ConvVAE(nn.Module):
         demo_input = torch.ones([1, self.img_channels, img_size, img_size])
         h_dim = self.encoder(demo_input).shape[1]
         print('h_dim', h_dim)
-        
+
         # map to latent z
         self.fc11 = nn.Linear(h_dim, self.z_dim)
         self.fc12 = nn.Linear(h_dim, self.z_dim)
@@ -53,53 +53,54 @@ class ConvVAE(nn.Module):
         # decoder
         self.fc2 = nn.Linear(self.z_dim, h_dim)
         self.decoder = self.get_decoder(filters_m, self.img_channels)
-        
+
         self.log_sigma = 0
         if self.model == 'sigma_vae':
             ## Sigma VAE
-            self.log_sigma = torch.nn.Parameter(torch.full((1,), 0)[0], requires_grad=args.model == 'sigma_vae')
-        
+            # self.log_sigma = torch.nn.Parameter(torch.full((1,), 0)[0], requires_grad=args.model == 'sigma_vae')
+            self.log_sigma = torch.nn.Parameter(torch.zeros((1,)[0]), requires_grad=args.model == 'sigma_vae')
+
     @staticmethod
     def get_encoder(img_channels, filters_m):
         return nn.Sequential(
-            nn.Conv2d(img_channels, filters_m, (3, 3), stride=1, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(filters_m, 2 * filters_m, (4, 4), stride=2, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(2 * filters_m, 4 * filters_m, (5, 5), stride=2, padding=2),
-            nn.ReLU(),
-            Flatten()
+                nn.Conv2d(img_channels, filters_m, (3, 3), stride=1, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(filters_m, 2 * filters_m, (4, 4), stride=2, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(2 * filters_m, 4 * filters_m, (5, 5), stride=2, padding=2),
+                nn.ReLU(),
+                Flatten()
         )
 
     @staticmethod
     def get_decoder(filters_m, out_channels):
         return nn.Sequential(
-            UnFlatten(4 * filters_m),
-            nn.ConvTranspose2d(4 * filters_m, 2 * filters_m, (6, 6), stride=2, padding=2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(2 * filters_m, filters_m, (6, 6), stride=2, padding=2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(filters_m, out_channels, (5, 5), stride=1, padding=2),
-            nn.Sigmoid(),
-        )
-        
+                UnFlatten(4 * filters_m),
+                nn.ConvTranspose2d(4 * filters_m, 2 * filters_m, (6, 6), stride=2, padding=2),
+                nn.ReLU(),
+                nn.ConvTranspose2d(2 * filters_m, filters_m, (6, 6), stride=2, padding=2),
+                nn.ReLU(),
+                nn.ConvTranspose2d(filters_m, out_channels, (5, 5), stride=1, padding=2),
+                nn.Sigmoid(),
+                )
+
     def encode(self, x):
         h = self.encoder(x)
         return self.fc11(h), self.fc12(h)
-    
+
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
         return mu + eps * std
-    
+
     def decode(self, z):
         return self.decoder(self.fc2(z))
-    
+
     def forward(self, x):
         mu, logvar = self.encode(x)
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
-    
+
     def sample(self, n):
         sample = torch.randn(n, self.z_dim).to(self.device)
         return self.decode(sample)
@@ -107,7 +108,7 @@ class ConvVAE(nn.Module):
     def reconstruction_loss(self, x_hat, x):
         """ Computes the likelihood of the data given the latent variable,
         in this case using a Gaussian distribution with mean predicted by the neural network and variance = 1 """
-        
+
         if self.model == 'gaussian_vae':
             # Naive gaussian VAE uses a constant variance
             log_sigma = torch.zeros([], device=x_hat.device)
@@ -123,9 +124,9 @@ class ConvVAE(nn.Module):
         # Learning the variance can become unstable in some cases. Softly limiting log_sigma to a minimum of -6
         # ensures stable training.
         log_sigma = softclip(log_sigma, -6)
-        
+
         rec = gaussian_nll(x_hat, log_sigma, x).sum()
-    
+
         return rec
 
     def loss_function(self, recon_x, x, mu, logvar):
@@ -135,13 +136,13 @@ class ConvVAE(nn.Module):
             rec = torch.nn.MSELoss()(recon_x, x)
         else:
             rec = self.reconstruction_loss(recon_x, x)
-        
+
         # see Appendix B from VAE paper:
         # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
         # https://arxiv.org/abs/1312.6114
         # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
         kl = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-        
+
         return rec, kl
 
 
--- train_vae.py
+++ train_vae.py
@@ -1,5 +1,8 @@
 import argparse
 import os
+import numpy as np
+from PIL import Image
+
 
 import torch
 from torch import optim
@@ -13,18 +16,44 @@ from model import ConvVAE
 """ This script is an example of Sigma VAE training in PyTorch. The code was adapted from:
 https://github.com/pytorch/examples/blob/master/vae/main.py """
 
+
+class NumpyDataset(torch.utils.data.Dataset):
+    def __init__(self, np_array, transform=None):
+        # self.array = (np_array * 255).astype(np.uint8)
+        self.array = np_array
+        # self.array = torch.from_numpy(np_array)
+        # self.array = Image.fromarray(np.uint8(np_array)).covert('RGB')
+        # __import__('ipdb').set_trace()
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.array)
+
+    def __getitem__(self, idx):
+        sample = self.array[idx]
+        print('before', sample.max())
+        test = np.uint8(sample * 25)
+        print('test', test.max())
+        sample = Image.fromarray(test, mode='RGB')
+        print('mid', np.max(np.array(sample)))
+        if self.transform:
+            sample = self.transform(sample)
+        print('after', np.max(np.array(sample)))
+        # sample += 0.5
+        return sample
+
 ## Arguments
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
 parser.add_argument('--batch-size', type=int, default=128, metavar='N',
-                    help='input batch size for training (default: 128)')
+        help='input batch size for training (default: 128)')
 parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
+        help='number of epochs to train (default: 10)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='enables CUDA training')
+        help='enables CUDA training')
 parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging training status')
+        help='how many batches to wait before logging training status')
 parser.add_argument('--model', type=str, default='mse', metavar='N',
-                    help='which model to use: mse_vae,  gaussian_vae, or sigma_vae or optimal_sigma_vae')
+        help='which model to use: mse_vae,  gaussian_vae, or sigma_vae or optimal_sigma_vae')
 parser.add_argument('--log_dir', type=str, default='test', metavar='N', required=True)
 args = parser.parse_args()
 
@@ -33,48 +62,85 @@ args.cuda = not args.no_cuda and torch.cuda.is_available()
 device = torch.device("cuda" if args.cuda else "cpu")
 
 ## Dataset
-transform = transforms.Compose([transforms.Resize((28, 28)), transforms.ToTensor()])
+kwargs = {'num_workers': 10, 'pin_memory': True} if args.cuda else {}
+img_size = 28
+
+
+
+
+transform = transforms.Compose([
+    # transforms.ToPILImage('RGB'),
+    transforms.Resize((28, 28)),
+    transforms.ToTensor(),
+])
 train_dataset = datasets.SVHN('../../data', split='train', download=True, transform=transform)
 test_dataset = datasets.SVHN('../../data', split='train', transform=transform)
-kwargs = {'num_workers': 10, 'pin_memory': True} if args.cuda else {}
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, **kwargs)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=True, **kwargs)
+print(train_dataset[0][0].max())
+print(train_dataset[0][0].shape)
 
+transform = transforms.Compose([
+    # transforms.ToPILImage('RGB'),
+    transforms.Resize((28, 28)),
+    transforms.ToTensor(),
+])
+train_path = '/home/vitchyr/mnt/log/manual-upload/sets/OneObject-PickAndPlace-BigBall-RandomInit-2D-v1-ungrouped-train-28x28.npy'
+test_path = '/home/vitchyr/mnt/log/manual-upload/sets/OneObject-PickAndPlace-BigBall-RandomInit-2D-v1-ungrouped-test-28x28.npy'
+test_imgs = np.load(test_path)
+# test_dataset = NumpyDataset(test_imgs, transform)
+test_dataset = torch.from_numpy(test_imgs.astype(np.float32))
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=True, **kwargs)
+
+train_imgs = np.load(train_path)
+# train_dataset = NumpyDataset(train_imgs, transform)
+train_dataset = torch.from_numpy(train_imgs.astype(np.float32))
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, **kwargs)
+
+# __import__('ipdb').set_trace()
 ## Logging
 os.makedirs('vae_logs/{}'.format(args.log_dir), exist_ok=True)
 summary_writer = SummaryWriter(log_dir='vae_logs/' + args.log_dir, purge_step=0)
 
 ## Build Model
-model = ConvVAE(device, 3, args).to(device)
+model = ConvVAE(device, 3, args, img_size=img_size).to(device)
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
 
 def train(epoch):
     model.train()
     train_loss = 0
-    for batch_idx, (data, _) in enumerate(train_loader):
+    for batch_idx, data in enumerate(train_loader):
+        if len(data) == 2:
+            data = data[0]
         data = data.to(device)
         optimizer.zero_grad()
-        
+
         # Run VAE
         recon_batch, mu, logvar = model(data)
         # Compute loss
         rec, kl = model.loss_function(recon_batch, data, mu, logvar)
-        
+
         total_loss = rec + kl
         total_loss.backward()
         train_loss += total_loss.item()
         optimizer.step()
-        
+
         if batch_idx % args.log_interval == 0:
+            log_sigma = model.log_sigma
+            if isinstance(log_sigma, torch.Tensor):
+                log_sigma = log_sigma.item()
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tMSE: {:.6f}\tKL: {:.6f}\tlog_sigma: {:f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
+                epoch,
+                batch_idx * len(data),
+                len(train_loader.dataset),
                 100. * batch_idx / len(train_loader),
                 rec.item() / len(data),
                 kl.item() / len(data),
-                model.log_sigma))
-            
-    train_loss /=  len(train_loader.dataset)
+                log_sigma,
+            ))
+
+            train_loss /=  len(train_loader.dataset)
     print('====> Epoch: {} Average loss: {:.4f}'.format(
         epoch, train_loss))
     summary_writer.add_scalar('train/elbo', train_loss, epoch)
@@ -87,7 +153,9 @@ def test(epoch):
     model.eval()
     test_loss = 0
     with torch.no_grad():
-        for i, (data, _) in enumerate(tqdm(test_loader)):
+        for i, data in enumerate(tqdm(test_loader)):
+            if len(data) == 2:
+                data = data[0]
             data = data.to(device)
             recon_batch, mu, logvar = model(data)
             # Pass the second value from posthoc VAE
@@ -97,7 +165,7 @@ def test(epoch):
                 n = min(data.size(0), 8)
                 comparison = torch.cat([data[:n], recon_batch.view(args.batch_size, -1, 28, 28)[:n]])
                 save_image(comparison.cpu(), 'vae_logs/{}/reconstruction_{}.png'.format(args.log_dir, str(epoch)), nrow=n)
-                
+
     test_loss /= len(test_loader.dataset)
     print('====> Test set loss: {:.4f}'.format(test_loss))
     summary_writer.add_scalar('test/elbo', test_loss, epoch)
@@ -110,7 +178,7 @@ if __name__ == "__main__":
         with torch.no_grad():
             sample = model.sample(64).cpu()
             save_image(sample.view(64, -1, 28, 28),
-                       'vae_logs/{}/sample_{}.png'.format(args.log_dir, str(epoch)))
-        summary_writer.file_writer.flush()
-        
+                    'vae_logs/{}/sample_{}.png'.format(args.log_dir, str(epoch)))
+            summary_writer.file_writer.flush()
+
     torch.save(model.state_dict(), 'vae_logs/{}/checkpoint_{}.pt'.format(args.log_dir, str(epoch)))
